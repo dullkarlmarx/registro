ea4bba1c9e5816b1857c1205d0cc4156a4fc2672
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-11 19:03:54 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -6,7 +6,7 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 const todo=document.getElementsByClassName("form-control")
-for (Por_cada of todo){
+for (por_cada of todo){
   if (por_cada.value.length !== 0){
     showAlertSuccess();
   }else{
