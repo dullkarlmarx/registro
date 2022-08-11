f5c47a13933c7143a1efe5050d4d5f2fe914120f
camilaabarno <110434722+camilaabarno@users.noreply.github.com>
2022-08-11 20:52:37 -0300

    cambios

--- js/validacion.js
+++ js/validacion.js
@@ -1,22 +1,47 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alertSuccess = () => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-success alert-dismissible" role="alert" id="alert-success">`,
+        `    <p>Datos guardados correctamente</p>`,
+        `    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
+        `</div>`
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
 }
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+const alertError = () => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-danger alert-dismissible" role="alert" id="alert-danger">`,
+        `    <p>Los datos ingresados no cumplen con los requisitos solicitados</p>`,
+        '    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+    alertPlaceholder.append(wrapper)
 }
+
+
+
+
 const todo = document.getElementsByClassName("form-control")
-
 
 let boton = document.getElementById("regBtn");
 boton.addEventListener("click", () => {
-    for (i = 0; i < todo.length; i++) {
-        if (por_cada.value.length !== 0) {
-            showAlertSuccess();
-        }
-        if (por_cada.value.length == 0) {
-            showAlertError();
+    let cantCompletas = 0;
+    for (por_cada of todo) {
+        
+        if (por_cada.value.length != 0) {
+            cantCompletas += 1;
         }
     }
-});
+    if (cantCompletas == 5) {
+        alertSuccess();
 
+    } else {
+        alertError();
+
+    }
+});
