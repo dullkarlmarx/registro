f27c77d639147451195eaefe7a8074ad6725ed96
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-11 23:08:12 -0300

    me puse al dia

--- js/validacion.js
+++ js/validacion.js
@@ -1,24 +1,29 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alertSuccess = () => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-success alert-dismissible" role="alert" id="alert-success">`,
+        `    <p>Datos guardados correctamente</p>`,
+        `    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
+        `</div>`
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
 }
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+const alertError = () => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-danger alert-dismissible" role="alert" id="alert-danger">`,
+        `    <p>Los datos ingresados no cumplen con los requisitos solicitados</p>`,
+        '    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+    alertPlaceholder.append(wrapper)
 }
 const todo = document.getElementsByClassName("form-control")
 
-/*
-let boton = document.getElementById("regBtn");
-boton.addEventListener("click", () => {
-    for (var i=0; i < todo.length; i++) {
-        if (por_cada.value.length !== 0) {
-            showAlertSuccess();
-        }
-        if (por_cada.value.length == 0) {
-            showAlertError();
-        }
-    }
-}); */
 let boton = document.getElementById("regBtn");
 boton.addEventListener("click", () => {
     let cantCompletas = 0;
@@ -29,8 +34,10 @@ boton.addEventListener("click", () => {
         }
     }
     if (cantCompletas == 5) {
-        showAlertSuccess();
+        alertSuccess();
+
     } else {
-        showAlertError();
+        alertError();
+
     }
 });
